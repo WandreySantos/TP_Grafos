e441ce8b6ee1cdc7d2cb0c2eced461816ed0226e
Wandrey <wandreymiceoni16@gmail.com>
2024-11-28 22:17:47 -0300

    Primeiro comit

---
+++ src/Grafo.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Grafo {
+    private List<Vertice> vertices;
+    private List<Aresta> arestas;
+
+    public Grafo(){
+        
+    }
+
+    public void inserirVertice(new Vertice()){
+
+    }
+    public Aresta inserirAresta(){
+
+    }
+}
---
+++ src/Vertice.java
@@ -0,0 +1,17 @@
+public class Vertice {
+    private String nome;
+
+    public Vertice(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+
+    
+}
