b639f426e34694e1d03fea801ffffa03177eacad
WandreySantos <wandreymiceoni16@gmail.com>
2024-11-29 20:22:35 -0300

    alteração geral

--- src/App.java
+++ src/App.java
@@ -2,138 +2,63 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    Scanner scan;
     public static void menu() {
         System.out.println("\n=== Menu Principal ===");
         System.out.println("1 - Arestas");
         System.out.println("2 - Ponderar arestas ");
         System.out.println("3 -  ");
-        System.out.println("9 - Ver matriz");
+        System.out.println("9 - Ver grafo");
         System.out.println("0 - Sair");
         System.out.println("==================");
     }
 
-    public static void mostrarMatrizAdj(int[][] matrizAdj) {
-        System.out.println("\nMatriz de Adjacência:");
-        int tamanho = matrizAdj.length;
-        System.out.print("    ");
-        for (int i = 0; i < tamanho; i++) {
-            System.out.print("v" + (i + 1) + "     ");
-        }
-        System.out.println();
-
-        for (int i = 0; i < tamanho; i++) {
-            System.out.print("v" + (i + 1) + "  ");
-            for (int j = 0; j < tamanho; j++) {
-                System.out.print("[" + matrizAdj[i][j] + "]" + "    ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void preencherMatrizAdj(int[][] matrizAdj, Scanner scan, boolean direcionado) {
-        int tamanho = matrizAdj.length;
-        System.out.println("Preencha a matriz de adjacência (0 para sem aresta e 1 para com aresta):");
-
-        for (int i = 0; i < tamanho; i++) {
-            for (int j = i; j < tamanho; j++) {
-                if (i == j && !direcionado) {
-                    continue;
-                }
-
-                int valor = -1;
-                // Garantir que o valor seja 0 ou 1
-                while (valor != 0 && valor != 1) {
-                    System.out.print("Matriz[" + i + "][" + j + "]: ");
-                    valor = scan.nextInt();
-                    if (valor != 0 && valor != 1) {
-                        System.out.println("Valor inválido! Por favor, insira 0 ou 1.");
-                    }
-                }
-                matrizAdj[i][j] = valor;
-
-                if (!direcionado && i != j) {
-                    matrizAdj[j][i] = matrizAdj[i][j];
-                }
-            }
-        }
-    }
-    
-    public static void addRemov(Grafo grafo, Scanner scan){
+    public static void addRemov(Grafo grafo, Scanner scan) {
         System.out.println("1- Adicionar\n2- Remover");
         int op = scan.nextInt();
         if (op == 1) {
             System.out.println("Selecione 2 vértices para adicionar uma aresta:");
             System.out.println("Vértices disponíveis: ");
             for (int i = 0; i < grafo.getVertices().size(); i++) {
-                System.out.println(i + ": " + grafo.getVertices().get(i));
+                System.out.println(i + ": " + grafo.getVertices().get(i).getNome());
             }
             System.out.print(": ");
             int index1 = scan.nextInt();
             System.out.print(": ");
             int index2 = scan.nextInt();
-            Vertice origem = grafo.getVertices().get(index1);
-            Vertice destino = grafo.getVertices().get(index2);
-            grafo.inserirAresta(origem, destino);
-        } else {
+
+            if (index1 >= 0 && index1 < grafo.getVertices().size() && index2 >= 0
+                    && index2 < grafo.getVertices().size()) {
+                Vertice origem = grafo.getVertices().get(index1);
+                Vertice destino = grafo.getVertices().get(index2);
+                grafo.inserirAresta(origem, destino);
+            } else {
+                System.out.println("Índices inválidos. Tente novamente.");
+            }
+        } else if (op == 2) {
             System.out.println("Qual aresta irá remover?");
             for (int i = 0; i < grafo.getArestas().size(); i++) {
-                System.out.println(i + ": " + grafo.getArestas().get(i));
+                System.out.println(i + ": " + grafo.getArestas().get(i).getNome());
             }
             System.out.print("Informe o índice da aresta a ser removida: ");
             int index = scan.nextInt();
 
             if (index >= 0 && index < grafo.getArestas().size()) {
                 Aresta aresta = grafo.getArestas().get(index);
-
                 grafo.removerAresta(aresta.getOrigem(), aresta.getDestino());
             } else {
                 System.out.println("Índice inválido. Aresta não encontrada.");
             }
+        } else {
+            System.out.println("Opção inválida.");
         }
     }
-    
-    public static int[][] crirarMatrizComGrafo(Grafo grafo) {
-        List<Vertice> vertices = grafo.getVertices();
-        int tamanho = vertices.size();
-        int[][] matrizAdj = new int[tamanho][tamanho];
 
-        for (int i = 0; i < tamanho; i++) {
-            for (int j = 0; j < tamanho; j++) {
-                matrizAdj[i][j] = 0;
-            }
-        }
-        for (Aresta aresta : grafo.getArestas()) {
-            int origem = vertices.indexOf(aresta.getOrigem());
-            int destino = vertices.indexOf(aresta.getDestino());
-            matrizAdj[origem][destino] = 1;
-            matrizAdj[destino][origem] = 1;
-        }
-        return matrizAdj;
-    }
-    public static Grafo criarGrafoComMatriz(int[][] matrizAdj, Grafo grafo) {
-        int tamanho = matrizAdj.length;
-
-        for (int i = 0; i < tamanho; i++) {
-            grafo.inserirVertice("v" + (i + 1));
-        }
-
-        for (int i = 0; i < tamanho; i++) {
-            for (int j = i + 1; j < tamanho; j++) {
-                if (matrizAdj[i][j] == 1) {
-                    grafo.inserirAresta(grafo.getVertices().get(i), grafo.getVertices().get(j));
-                }
-            }
-        }
-        return grafo;
-    }
-    
-    public static Grafo ponderarArestas(Grafo grafo, Scanner scan){
+    public static Grafo ponderarArestas(Grafo grafo, Scanner scan) {
         List<Aresta> arestas = grafo.getArestas();
         System.out.println(arestas);
         int i = 0;
         for (Aresta aresta : arestas) {
-            System.out.println("Qual o peso da aresta " + aresta.getNome()+ "?");
+            System.out.println("Qual o peso da aresta " + aresta.getNome() + "?");
             int peso = scan.nextInt();
             aresta.setPeso(i); // Define o peso como o índice
             arestas.get(i).setPeso(peso);
@@ -141,20 +66,16 @@ public class App {
         }
         return grafo;
     }
+
     public static void main(String[] args) throws Exception {
         boolean saida = true;
         Scanner scan = new Scanner(System.in);
-        Grafo grafo = new Grafo();
 
         System.out.println("Tamanho do grafo: ");
         int tamanho = scan.nextInt();
 
-        int[][] matrizAdj = new int[tamanho][tamanho];
-        preencherMatrizAdj(matrizAdj, scan, false);
+        Grafo grafo = new Grafo(tamanho);
 
-        grafo = criarGrafoComMatriz(matrizAdj, grafo);
-
-        mostrarMatrizAdj(matrizAdj);
         // mostrarListaAdj();
 
         // Menu principal
@@ -167,16 +88,14 @@ public class App {
                     addRemov(grafo, scan);
                     break;
                 case 2:
-                    ponderarArestas(grafo,scan);
+                    ponderarArestas(grafo, scan);
                     break;
                 case 3:
-                    
+
                     break;
 
                 case 9:
-                    matrizAdj = crirarMatrizComGrafo(grafo);
-                    System.out.print(grafo.toString());
-                    mostrarMatrizAdj(matrizAdj);
+                    System.out.println(grafo.toString());
                     break;
                 case 0:
                     saida = false;
--- src/Grafo.java
+++ src/Grafo.java
@@ -1,73 +1,165 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Grafo {
     private List<Vertice> vertices;
     private List<Aresta> arestas;
     public int tamanho;
+    private boolean direcionado = false;
+    private int[][] matrizAdj;
 
-    public Grafo(){
+    Scanner scan = new Scanner(System.in);
+
+    public Grafo(int tamanho) {
         this.vertices = new ArrayList<>();
         this.arestas = new ArrayList<>();
-        this.tamanho=vertices.size();
+        this.tamanho = tamanho;
+
+        // Inicializa a matriz de adjacência
+        this.matrizAdj = new int[tamanho][tamanho];
+
+        preencherMatriz();
+        mostrarMatrizAdj();
+        criandoGrafo();
     }
-    public void inserirVertice(String nome){
-        Vertice vertice  = new Vertice(nome);
+
+    private void preencherMatriz() {
+        System.out.println("Preencha a matriz de adjacência (0 para sem aresta, 1 para com aresta):");
+        for (int i = 0; i < tamanho; i++) {
+            for (int j = i + 1; j < tamanho; j++) { 
+                int valor = -1;
+                while (valor != 0 && valor != 1) {
+                    System.out.print("Matriz[" + i + "][" + j + "]: ");
+                    valor = scan.nextInt();
+                    if (valor != 0 && valor != 1) {
+                        System.out.println("Valor inválido! Por favor, insira 0 ou 1.");
+                    }
+                }
+                // Preenche a diagonal superior
+                matrizAdj[i][j] = valor;
+                // Espelha na diagonal inferior
+                matrizAdj[j][i] = valor;
+            }
+        }
+    }
+
+    private void criandoGrafo() {
+        // A matriz de adjacência já foi preenchida pela função `preenchendoMatriz`
+        for (int i = 0; i < tamanho; i++) {
+            inserirVertice("v" + (i + 1));
+        }
+        for (int i = 0; i < tamanho; i++) {
+            for (int j = i + 1; j < tamanho; j++) {
+                if (matrizAdj[i][j] == 1) {
+                    inserirAresta(vertices.get(i), vertices.get(j));
+                }
+            }
+        }
+    }
+
+    public Grafo inserirVertice(String nome) {
+        Vertice vertice = new Vertice(nome);
         vertices.add(vertice);
+        return this;
     }
-    public void removVertice(int index){
+
+    public Grafo removerVertice(int index) {
         vertices.remove(index);
+        return this;
     }
 
     public void inserirAresta(Vertice origem, Vertice destino) {
-        String nome = origem.getNome()+"-> "+destino.getNome();
         for (Aresta aresta : arestas) {
             if (aresta.getOrigem().equals(origem) && aresta.getDestino().equals(destino)) {
                 System.out.println("Aresta " + origem + " -> " + destino + " já existe.");
-                return;
             }
         }
-
+        String nome = origem.getNome() + " -> " + destino.getNome();
         Aresta aresta = new Aresta(origem, destino, nome);
         arestas.add(aresta);
         System.out.println("Aresta " + origem + " -> " + destino + " inserida.");
+        System.out.println(arestas);
     }
-    
-    public void removerAresta(Vertice origem, Vertice destino){
+
+    public Grafo removerAresta(Vertice origem, Vertice destino) {
         Aresta aresta = new Aresta(origem, destino);
         if (arestas.remove(aresta)) {
-            System.out.println("Aresta " + origem + " -> " + destino + " removida.");
+            int i = vertices.indexOf(origem);
+            int j = vertices.indexOf(destino);
+            matrizAdj[i][j] = 0;
+            if (!direcionado) {
+                matrizAdj[j][i] = 0;
+            }
+            System.out.println("Aresta " + origem.getNome() + " -> " + destino.getNome() + " removida.");
         } else {
-            System.out.println("Aresta " + origem + " -> " + destino + " não encontrada.");
+            System.out.println("Aresta " + origem.getNome() + " -> " + destino.getNome() + " não encontrada.");
         }
+        return this;
     }
 
     public List<Vertice> getVertices() {
         return vertices;
     }
+
     public List<Aresta> getArestas() {
         return arestas;
     }
 
+    public void criarGrafoComMatriz() {
+        tamanho = matrizAdj.length;
+
+        for (int i = 0; i < tamanho; i++) {
+            inserirVertice("v" + (i + 1));
+        }
+
+        for (int i = 0; i < tamanho; i++) {
+            for (int j = i + 1; j < tamanho; j++) {
+                if (matrizAdj[i][j] == 1) {
+                    inserirAresta(getVertices().get(i), getVertices().get(j));
+                }
+            }
+        }
+    }
+
+    public void mostrarMatrizAdj() {
+        System.out.println("\nMatriz de Adjacência:");
+        System.out.print("    ");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print("v" + (i + 1) + "     ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print("v" + (i + 1) + "  ");
+            for (int j = 0; j < tamanho; j++) {
+                System.out.print("[" + matrizAdj[i][j] + "]" + "    ");
+            }
+            System.out.println();
+        }
+    }
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Vértices:\n");
-        for (Vertice vertice : vertices) {
-            sb.append(vertice.getNome()).append("\n");
-        }
-        sb.append("\nArestas:\n");
+        sb.append("Arestas do Grafo:\n");
         for (Aresta aresta : arestas) {
-            sb.append(aresta.getOrigem().getNome())
-            .append(" -> ")
-            .append(aresta.getDestino().getNome());
-
-            if (aresta.getPeso() != 0) {
-                sb.append(" [Peso: ").append(aresta.getPeso()).append("]");
+            sb.append(aresta).append("\n");
+        }
+        sb.append("\nMatriz de Adjacência:\n");
+        sb.append("    ");
+        for (int i = 0; i < tamanho; i++) {
+            sb.append("v").append(i + 1).append("     ");
+        }
+        sb.append("\n");
+        for (int i = 0; i < tamanho; i++) {
+            sb.append("v").append(i + 1).append("  ");
+            for (int j = 0; j < tamanho; j++) {
+                sb.append("[").append(matrizAdj[i][j]).append("]    ");
             }
             sb.append("\n");
         }
         return sb.toString();
     }
+    
 
 }
